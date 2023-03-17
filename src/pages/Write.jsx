47d177fd9b300f2de4b8b1f489dfd00875e2c498
Inseo Yang <y.1nseooo@gmail.com>
2023-03-17 15:18:50 +0900

    add write page

---
+++ src/pages/Write.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Wrapper from "../components/Wrapper";
+import GlobalStyle from "../GlobalStyle";
+
+function Write() {
+  return (
+    <Wrapper>
+      <GlobalStyle />
+      Write
+    </Wrapper>
+  );
+}
+
+export default Write;
