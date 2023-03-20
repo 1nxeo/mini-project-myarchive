b28f87038cf8969b534b9374ff546fe29c6ed72c
Inseo Yang <y.1nseooo@gmail.com>
2023-03-20 09:56:53 +0900

    style: Home에 카테고리 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import Header from '../components/Header'
-import Nav from '../components/Nav'
-import Wrapper from '../components/Wrapper'
-import GlobalStyle from '../GlobalStyle'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { __getPost } from '../redux/modules/postSlice'
-import { useEffect } from 'react'
-import Card from '../components/Card'
-import Button from '../components/Button'
+import React from "react";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Wrapper from "../components/Wrapper";
+import GlobalStyle from "../GlobalStyle";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { __getPost } from "../redux/modules/postSlice";
+import { useEffect } from "react";
+import Card from "../components/Card";
+import Button from "../components/Button";
+import Category from "../components/Category";
 
 function Home() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const { posts, isLoading, error } = useSelector((state) => state.posts)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { posts, isLoading, error } = useSelector((state) => state.posts);
 
   useEffect(() => {
-    dispatch(__getPost())
-  }, [])
+    dispatch(__getPost());
+  }, []);
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   if (error) {
-    return <div>{error.message}</div>
+    return <div>{error.message}</div>;
   }
 
   return (
@@ -34,22 +35,17 @@ function Home() {
       <Header />
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
         }}
       >
-        <select>
-          <option>카테고리1</option>
-          <option>카테고리2</option>
-          <option>카테고리3</option>
-          <option>카테고리4</option>
-        </select>
-        <Button onClick={() => navigate('/post')}>글쓰기</Button>
+        <Category />
+        <Button onClick={() => navigate("/post")}>글쓰기</Button>
       </div>
       <Card post={posts} />
     </Wrapper>
-  )
+  );
 }
 
-export default Home
+export default Home;
