ef559058baba46a8eb78ca087bfc74eccae290f5
Inseo Yang <y.1nseooo@gmail.com>
2023-03-20 18:59:12 +0900

    [feat]홈화면 버튼추가

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "./Button";
+import { changeCates } from "../redux/modules/cateSlice";
 
 function Category() {
   return (
     <StCateBar>
       <StButtonBox>
         <Button>전체</Button>
-        <Button>카테고리 1</Button>
-        <Button>카테고리 2</Button>
-        <Button>카테고리 3</Button>
-        <Button>카테고리 4</Button>
+        <Button value="cate1" onClick={(e) => changeCates(e.target.value)}>
+          카테고리 1
+        </Button>
+        <Button value="cate2" onClick={(e) => changeCates(e.target.value)}>
+          카테고리 2
+        </Button>
+        <Button value="cate3" onClick={(e) => changeCates(e.target.value)}>
+          카테고리 3
+        </Button>
+        <Button value="cate4" onClick={(e) => changeCates(e.target.value)}>
+          카테고리 4
+        </Button>
       </StButtonBox>
       <StButtonBox>
         <Button>구매한 아이템</Button>
