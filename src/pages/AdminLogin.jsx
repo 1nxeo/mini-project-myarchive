180edcc46c02112bd847aa19cedf1ca661380dec
choidami5126 <dahyun5003@kakao.com>
2023-03-21 02:37:04 +0900

    [build] 신규 페이지 추가

---
+++ src/pages/AdminLogin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Wrapper from '../components/Wrapper'
+
+function AdminLogin() {
+  return (
+    <Wrapper>
+      AdminLogin
+      <></>
+    </Wrapper>
+  )
+}
+
+export default AdminLogin
