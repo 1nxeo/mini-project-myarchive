a9db0cbf2305b04b8c69c84585af6c69ef27a7be
choidami5126 <dahyun5003@kakao.com>
2023-03-21 23:04:50 +0900

    [feat] 관리자 페이지 조회 기능 구현

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -4,9 +4,17 @@ import postSlice from '../modules/postSlice'
 import memberSlice from '../modules/memberSlice'
 import detailSlice from '../modules/detailSlice'
 import cateSlice from '../modules/cateSlice'
+import adminSlice from '../modules/adminSlice'
 
 const store = configureStore({
-  reducer: { posts: postSlice, users: userSlice, members:memberSlice, details:detailSlice, cate:cateSlice },
+  reducer: {
+    posts: postSlice,
+    users: userSlice,
+    members: memberSlice,
+    details: detailSlice,
+    cate: cateSlice,
+    admins: adminSlice,
+  },
 })
 
 export default store
