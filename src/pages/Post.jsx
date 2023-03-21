0ac1af9f8afb2da775a681f22d65e93eeaa6f862
choidami5126 <dahyun5003@kakao.com>
2023-03-21 17:12:41 +0900

    [docs] 주석 오타 수정, onChange 함수 변수명 변경

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,66 +1,66 @@
-import React, { useEffect, useState } from "react";
-import Wrapper from "../components/Wrapper";
-import GlobalStyle from "../GlobalStyle";
-import styled from "styled-components";
-import Input from "../components/Input";
-import Button from "../components/Button";
-import Nav from "../components/Nav";
-import { __addPost } from "../redux/modules/postSlice";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { cookies } from "../shared/cookies";
-import Dropdown from "../components/Dropdown";
+import React, { useEffect, useState } from 'react'
+import Wrapper from '../components/Wrapper'
+import GlobalStyle from '../GlobalStyle'
+import styled from 'styled-components'
+import Input from '../components/Input'
+import Button from '../components/Button'
+import Nav from '../components/Nav'
+import { __addPost } from '../redux/modules/postSlice'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { cookies } from '../shared/cookies'
+import Dropdown from '../components/Dropdown'
 
 function Post() {
-  const token = cookies.get("token");
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const token = cookies.get('token')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   // input state를 한번에 관리함
   const [posts, setPosts] = useState({
     // accountId: '',
-    url: "",
-    title: "",
-    category: "category",
-    desc: "",
+    url: '',
+    title: '',
+    category: 'category',
+    desc: '',
     isDone: false,
-  });
+  })
 
   useEffect(() => {
     if (!token) {
-      alert("로그인이 필요합니다!");
-      navigate("/login");
+      alert('로그인이 필요합니다!')
+      navigate('/login')
     }
-    return () => {};
-  }, []);
+    return () => {}
+  }, [])
 
-  console.log(posts.category);
+  console.log(posts.category)
 
   // input onChange를 한번에 관리함
-  const changeInputHandler = (event) => {
-    const { value, name } = event.target;
+  const inputOnChangeHandler = (event) => {
+    const { value, name } = event.target
     setPosts((old) => {
-      return { ...old, [name]: value };
-    });
-  };
+      return { ...old, [name]: value }
+    })
+  }
 
-  // Psots 추가 함수
+  // Posts 추가 함수
   const postsButtonClickHandler = async (e) => {
-    e.preventDefault();
-    if (posts.category !== "category") {
-      await dispatch(__addPost({ posts: posts, next: () => navigate("/") }));
+    e.preventDefault()
+    if (posts.category !== 'category') {
+      await dispatch(__addPost({ posts: posts, next: () => navigate('/') }))
       // input 칸을 리셋함
       setPosts({
-        url: "",
-        title: "",
-        category: "category",
-        desc: "",
+        url: '',
+        title: '',
+        category: 'category',
+        desc: '',
         isDone: false,
-      });
+      })
     } else {
-      alert("입력한 내용을 확인해주세요!");
+      alert('입력한 내용을 확인해주세요!')
     }
-  };
+  }
 
   return (
     <Wrapper>
@@ -69,21 +69,11 @@ function Post() {
       Post
       <StForm onSubmit={postsButtonClickHandler}>
         URL
-        <Input
-          type="text"
-          name="url"
-          value={posts.url}
-          onChange={changeInputHandler}
-          required
-        ></Input>
+        <Input type="text" name="url" value={posts.url} onChange={changeInputHandler} required></Input>
         <br />
         카테고리
         {/* <Dropdown key={posts.id} item={posts} setItem={setPosts} /> */}
-        <select
-          name="category"
-          value={posts.category}
-          onChange={changeInputHandler}
-        >
+        <select name="category" value={posts.category} onChange={inputOnChangeHandler}>
           <option>Category</option>
           <option value="cloth">Clothes</option>
           <option value="it">IT</option>
@@ -94,31 +84,19 @@ function Post() {
         </select>
         <br />
         제목
-        <Input
-          type="text"
-          name="title"
-          value={posts.title}
-          onChange={changeInputHandler}
-          required
-        ></Input>
+        <Input type="text" name="title" value={posts.title} onChange={inputOnChangeHandler} required></Input>
         <br />
         내용
-        <Input
-          type="text"
-          name="desc"
-          value={posts.desc}
-          onChange={changeInputHandler}
-          required
-        ></Input>
+        <Input type="text" name="desc" value={posts.desc} onChange={inputOnChangeHandler} required></Input>
         <br />
         <Button>작성하기</Button>
-        <Button onClick={() => navigate("/")}>뒤로가기</Button>
+        <Button onClick={() => navigate('/')}>뒤로가기</Button>
       </StForm>
     </Wrapper>
-  );
+  )
 }
 
-export default Post;
+export default Post
 
 const StForm = styled.form`
   width: 1000px;
@@ -126,4 +104,4 @@ const StForm = styled.form`
 
   margin: 30px auto 30px auto;
   border: 1px solid black;
-`;
+`
