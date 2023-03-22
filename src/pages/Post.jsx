b7a157013473542760b58215103b3c947f67c2f2
choidami5126 <dahyun5003@kakao.com>
2023-03-22 15:37:55 +0900

    [fix] 유저 페이지에 입장 시 관리자 토큰 삭제 되도록 수정

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,69 +1,72 @@
-import React, { useEffect, useState } from "react";
-import Wrapper from "../components/Wrapper";
-import GlobalStyle from "../GlobalStyle";
-import styled from "styled-components";
-import Input from "../components/Input";
-import Button from "../components/Button";
-import Nav from "../components/Nav";
-import { __addPost } from "../redux/modules/postSlice";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { cookies } from "../shared/cookies";
-import WinWrapper from "../components/WinWrapper";
-import { changeCates } from "../redux/modules/cateSlice";
+import React, { useEffect, useState } from 'react'
+import Wrapper from '../components/Wrapper'
+import GlobalStyle from '../GlobalStyle'
+import styled from 'styled-components'
+import Input from '../components/Input'
+import Button from '../components/Button'
+import Nav from '../components/Nav'
+import { __addPost } from '../redux/modules/postSlice'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { cookies } from '../shared/cookies'
+import WinWrapper from '../components/WinWrapper'
+import { changeCates } from '../redux/modules/cateSlice'
 
 function Post() {
-  const token = cookies.get("token");
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const token = cookies.get('token')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   // input state를 한번에 관리함
   const [posts, setPosts] = useState({
     // accountId: '',
-    url: "",
-    title: "",
-    category: "category",
-    desc: "",
+    url: '',
+    title: '',
+    category: 'category',
+    desc: '',
     isDone: false,
-  });
+  })
 
   useEffect(() => {
+    if (cookies.get('adminToken')) {
+      cookies.remove('adminToken')
+    }
     if (!token) {
-      alert("로그인이 필요합니다!");
-      navigate("/login");
+      alert('로그인이 필요합니다!')
+      navigate('/login')
     }
     return () => {
-      dispatch(changeCates("notdone"));
-    };
-  }, []);
+      dispatch(changeCates('notdone'))
+    }
+  }, [])
 
-  console.log(posts.category);
+  console.log(posts.category)
 
   // input onChange를 한번에 관리함
   const changeInputHandler = (event) => {
-    const { value, name } = event.target;
+    const { value, name } = event.target
     setPosts((old) => {
-      return { ...old, [name]: value };
-    });
-  };
+      return { ...old, [name]: value }
+    })
+  }
 
   // Posts 추가 함수
   const postsButtonClickHandler = async (e) => {
-    e.preventDefault();
-    if (posts.category !== "category") {
-      await dispatch(__addPost({ posts: posts, next: () => navigate("/") }));
+    e.preventDefault()
+    if (posts.category !== 'category') {
+      await dispatch(__addPost({ posts: posts, next: () => navigate('/') }))
       // input 칸을 리셋함
       setPosts({
-        url: "",
-        title: "",
-        category: "category",
-        desc: "",
+        url: '',
+        title: '',
+        category: 'category',
+        desc: '',
         isDone: false,
-      });
+      })
     } else {
-      alert("입력한 내용을 확인해주세요!");
+      alert('입력한 내용을 확인해주세요!')
     }
-  };
+  }
 
   return (
     <Wrapper>
@@ -71,20 +74,10 @@ function Post() {
       <WinWrapper>
         <StForm onSubmit={postsButtonClickHandler}>
           URL
-          <Input
-            type="text"
-            name="url"
-            value={posts.url}
-            onChange={changeInputHandler}
-            required
-          ></Input>
+          <Input type="text" name="url" value={posts.url} onChange={changeInputHandler} required></Input>
           <br />
           카테고리
-          <select
-            name="category"
-            value={posts.category}
-            onChange={changeInputHandler}
-          >
+          <select name="category" value={posts.category} onChange={changeInputHandler}>
             <option>Category</option>
             <option value="cloth">Clothes</option>
             <option value="it">IT</option>
@@ -95,32 +88,20 @@ function Post() {
           </select>
           <br />
           제목
-          <Input
-            type="text"
-            name="title"
-            value={posts.title}
-            onChange={changeInputHandler}
-            required
-          ></Input>
+          <Input type="text" name="title" value={posts.title} onChange={changeInputHandler} required></Input>
           <br />
           내용
-          <Input
-            type="text"
-            name="desc"
-            value={posts.desc}
-            onChange={changeInputHandler}
-            required
-          ></Input>
+          <Input type="text" name="desc" value={posts.desc} onChange={changeInputHandler} required></Input>
           <br />
           <button>작성하기</button>
-          <button onClick={() => navigate("/")}>뒤로가기</button>
+          <button onClick={() => navigate('/')}>뒤로가기</button>
         </StForm>
       </WinWrapper>
     </Wrapper>
-  );
+  )
 }
 
-export default Post;
+export default Post
 
 const StForm = styled.form`
   width: 1000px;
@@ -128,4 +109,4 @@ const StForm = styled.form`
 
   margin: 30px auto 30px auto;
   border: 1px solid black;
-`;
+`
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,87 +1,78 @@
-import React, { useState, useEffect } from "react";
-import Header from "../components/Header";
-import Nav from "../components/Nav";
-import Wrapper from "../components/Wrapper";
-import GlobalStyle from "../GlobalStyle";
-import styled from "styled-components";
-import Input from "../components/Input";
-import Button from "../components/Button";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  __addUsers,
-  __checkUserId,
-  __checkUserNick,
-  __loginUser,
-} from "../redux/modules/userSlice";
-import { useNavigate } from "react-router-dom";
-import WinWrapper from "../components/WinWrapper";
+import React, { useState, useEffect } from 'react'
+import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Wrapper from '../components/Wrapper'
+import GlobalStyle from '../GlobalStyle'
+import styled from 'styled-components'
+import Input from '../components/Input'
+import Button from '../components/Button'
+import { useDispatch, useSelector } from 'react-redux'
+import { __addUsers, __checkUserId, __checkUserNick, __loginUser } from '../redux/modules/userSlice'
+import { useNavigate } from 'react-router-dom'
+import WinWrapper from '../components/WinWrapper'
 
 function Register() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const users = useSelector((state) => state.users)
 
-  // useEffect(() => {
-  //   console.log("hello world!");
-  // }, []);
+  useEffect(() => {
+    if (cookies.get('adminToken')) {
+      cookies.remove('adminToken')
+    }
+  }, [])
 
   // 필요한 state 설정
   const [newUser, setNewUser] = useState({
-    accountId: "",
-    password: "",
-    nick: "",
-  });
+    accountId: '',
+    password: '',
+    nick: '',
+  })
 
   // 중복확인용 state들
-  const [validId, setValidId] = useState(false);
-  const [validNick, setValidNick] = useState(false);
-  const [rePw, setRePw] = useState("");
+  const [validId, setValidId] = useState(false)
+  const [validNick, setValidNick] = useState(false)
+  const [rePw, setRePw] = useState('')
 
   // 정규표현식 - id, pw 유효성
   const checkValidId = (item) => {
     // id 영문소문자, 숫자, 4자리 이상
-    const regEx = /^[a-z0-9]{4,}$/;
-    return regEx.test(item);
-  };
+    const regEx = /^[a-z0-9]{4,}$/
+    return regEx.test(item)
+  }
 
   // 아이디 중복확인
   const checkIdHandler = (accId) => {
-    dispatch(__checkUserId(accId));
-    setValidId(true);
-  };
+    dispatch(__checkUserId(accId))
+    setValidId(true)
+  }
 
   // 닉네임 중복확인
   const checkNickHandler = (nick) => {
-    dispatch(__checkUserNick(nick));
-    setValidNick(true);
-  };
+    dispatch(__checkUserNick(nick))
+    setValidNick(true)
+  }
 
   // 비밀번호 중복 확인
   const checkSamePwHandler = (item) => {
-    return newUser.password !== item ? false : true;
-  };
+    return newUser.password !== item ? false : true
+  }
 
   //회원가입 버튼
   const addUserHandler = async (e) => {
-    e.preventDefault();
-    if (
-      validId &&
-      validNick &&
-      checkSamePwHandler(rePw) &&
-      checkValidId(newUser.accountId) &&
-      checkValidId(newUser.password)
-    ) {
+    e.preventDefault()
+    if (validId && validNick && checkSamePwHandler(rePw) && checkValidId(newUser.accountId) && checkValidId(newUser.password)) {
       try {
-        await dispatch(__addUsers(newUser));
+        await dispatch(__addUsers(newUser))
         // await dispatch(__loginUser(newUser));
-        return navigate("/login");
+        return navigate('/login')
       } catch (err) {
-        return alert("입력한 정보를 확인해주세요");
+        return alert('입력한 정보를 확인해주세요')
       }
     } else {
-      alert("입력한 정보를 확인해주세요");
+      alert('입력한 정보를 확인해주세요')
     }
-  };
+  }
 
   return (
     <Wrapper>
@@ -90,7 +81,7 @@ function Register() {
         <Nav />
         <form
           onSubmit={(e) => {
-            addUserHandler(e);
+            addUserHandler(e)
           }}
         >
           <FormWrapper>
@@ -99,35 +90,19 @@ function Register() {
               required
               type="text"
               value={newUser.accountId}
-              onChange={(e) =>
-                setNewUser({ ...newUser, accountId: e.target.value })
-              }
+              onChange={(e) => setNewUser({ ...newUser, accountId: e.target.value })}
             />
             {checkValidId(newUser.accountId) ? null : (
-              <span style={{ color: "red" }}>
-                아이디는 영문 소문자, 숫자로 4자리 이상이어야합니다.
-              </span>
+              <span style={{ color: 'red' }}>아이디는 영문 소문자, 숫자로 4자리 이상이어야합니다.</span>
             )}
-            <button
-              type="button"
-              style={{ width: "80px" }}
-              onClick={() => checkIdHandler({ accountId: newUser.accountId })}
-            >
+            <button type="button" style={{ width: '80px' }} onClick={() => checkIdHandler({ accountId: newUser.accountId })}>
               중복확인
             </button>
           </FormWrapper>
           <FormWrapper>
             <label>nickname:</label>
-            <Input
-              type="text"
-              value={newUser.nick}
-              onChange={(e) => setNewUser({ ...newUser, nick: e.target.value })}
-            />
-            <button
-              type="button"
-              style={{ width: "80px" }}
-              onClick={() => checkNickHandler({ nick: newUser.nick })}
-            >
+            <Input type="text" value={newUser.nick} onChange={(e) => setNewUser({ ...newUser, nick: e.target.value })} />
+            <button type="button" style={{ width: '80px' }} onClick={() => checkNickHandler({ nick: newUser.nick })}>
               중복확인
             </button>
           </FormWrapper>
@@ -137,14 +112,10 @@ function Register() {
               required
               type="password"
               value={newUser.password}
-              onChange={(e) =>
-                setNewUser({ ...newUser, password: e.target.value })
-              }
+              onChange={(e) => setNewUser({ ...newUser, password: e.target.value })}
             />
             {checkValidId(newUser.password) ? null : (
-              <span style={{ color: "red" }}>
-                비밀번호는 영문소문자, 숫자로 4자리 이상이어야합니다.
-              </span>
+              <span style={{ color: 'red' }}>비밀번호는 영문소문자, 숫자로 4자리 이상이어야합니다.</span>
             )}
           </FormWrapper>
           <FormWrapper>
@@ -153,7 +124,7 @@ function Register() {
               type="password"
               value={rePw}
               onChange={(e) => {
-                setRePw(e.target.value);
+                setRePw(e.target.value)
               }}
             />
           </FormWrapper>
@@ -161,24 +132,22 @@ function Register() {
             checkSamePwHandler(rePw) ? (
               <span>비밀번호가 일치합니다.</span>
             ) : (
-              <span style={{ color: "red" }}>
-                비밀번호가 일치하지 않습니다.
-              </span>
+              <span style={{ color: 'red' }}>비밀번호가 일치하지 않습니다.</span>
             )
           ) : null}
-          <button type="submit" style={{ width: "100px" }}>
+          <button type="submit" style={{ width: '100px' }}>
             회원가입
           </button>
         </form>
       </WinWrapper>
     </Wrapper>
-  );
+  )
 }
 
 export const FormWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 400px;
-`;
+`
 
-export default Register;
+export default Register
