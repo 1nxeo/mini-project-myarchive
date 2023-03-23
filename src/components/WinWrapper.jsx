a4e5ae9038c0e4bf152d2bc675c3f3afc4be7d37
Inseo Yang <y.1nseooo@gmail.com>
2023-03-23 15:35:21 +0900

    [style] css 수정 제발 애들아 창밖으로 나오지좀마

--- src/components/WinWrapper.jsx
+++ src/components/WinWrapper.jsx
@@ -21,7 +21,7 @@ function WinWrapper({ children, ...rest }) {
         style={{
           maxWidth: "1200px",
           minWidth: " 900px",
-          maxHeight: "90vh",
+          maxHeight: "900px",
           marginTop: "60px",
           minHeight: "700px",
         }}
